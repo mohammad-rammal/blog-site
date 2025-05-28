621865f962bcc42b7f0083abe7d80f4117be0722
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-05-28 22:20:11 +0300

    Reset Password Backend

--- backend/app.js
+++ backend/app.js
@@ -38,6 +38,6 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT} ⚙️`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}  ⚙️`
   )
 );
