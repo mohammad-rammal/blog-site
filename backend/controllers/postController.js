0077d7368ffb6bba36410bb78064b7f64a454b13
mohammad-rammal <mohammad.rammal@hotmail.com>
2025-05-29 02:14:49 +0300

    Add Populate For PostController

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -186,7 +186,9 @@ module.exports.updatePostCtrl = asyncHandler(async (req, res) => {
       },
     },
     { new: true }
-  ).populate("user", ["-password"]);
+  )
+    .populate("user", ["-password"])
+    .populate("comments");
 
   // 5- send response to the client
   res.status(200).json(updatedPost);
